981f709371c7a3f2ac841d095cd96880fe0f7945
rxavio <rucahoxavi@gmail.com>
2022-04-09 20:34:30 +0200

    update main urls

--- medium/urls.py
+++ medium/urls.py
@@ -13,23 +13,45 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+# from reviews.views import ProductViewSet, ImageViewSet, MyObtainTokenPairView
+# from rest_framework.routers import DefaultRouter
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from rest_framework_simplejwt.views import TokenRefreshView
+
+# router = DefaultRouter()
+# router.register(r'products', ProductViewSet, basename='Product')
+# router.register(r'image', ImageViewSet, basename='Image')
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('token/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+#     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#     path('api/', include(router.urls)),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 from django.contrib import admin
 from django.urls import path, include
-from reviews.views import ProductViewSet, ImageViewSet, MyObtainTokenPairView
+from reviews.views import ProductViewSet, ImageViewSet
 from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenRefreshView
+
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet, basename='Product')
+router.register(r'product', ProductViewSet, basename='Product')
 router.register(r'image', ImageViewSet, basename='Image')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('token/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(router.urls)),
+    path('auth/', include('auth.urls')),
+    path('', include(router.urls)),
 ]
 
 if settings.DEBUG:
