d646fac6e602767f9dc7eb6050495e72364f70d0
rxavio <rucahoxavi@gmail.com>
2022-04-09 20:31:28 +0200

    add register view in auth

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
